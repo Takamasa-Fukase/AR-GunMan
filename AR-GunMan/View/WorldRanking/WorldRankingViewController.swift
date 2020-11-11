5312ef539c17dc51abcdfa191f8f6ab8a5b948da
深瀬 貴将 <fukase@arsaga.jp>
2020-11-11 10:05:51 +0900

    add WorldRankingVC

---
+++ AR-GunMan/View/WorldRanking/WorldRankingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WorldRankingViewController.swift
+//  AR-GunMan
+//
+//  Created by 深瀬 貴将 on 2020/11/11.
+//
+
+import UIKit
+
+class WorldRankingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
