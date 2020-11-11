3433eefb00ec69cc31b74bb49073b89a9b473378
深瀬 貴将 <fukase@arsaga.jp>
2020-11-11 10:06:22 +0900

    add tableView method

--- AR-GunMan/View/WorldRanking/WorldRankingViewController.swift
+++ AR-GunMan/View/WorldRanking/WorldRankingViewController.swift
@@ -6,24 +6,40 @@
 //
 
 import UIKit
+import Firebase
+
+struct Ranking {
+    let score: String
+    let userName: String
+}
 
 class WorldRankingViewController: UIViewController {
 
+    var db: Firestore!
+    var list: [Ranking] = []
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension WorldRankingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
+        cell.textLabel?.text = list[indexPath.row].userName
+        cell.detailTextLabel?.text = list[indexPath.row].score
+        return cell
+    }
+    
+    
+    
+    
 }
