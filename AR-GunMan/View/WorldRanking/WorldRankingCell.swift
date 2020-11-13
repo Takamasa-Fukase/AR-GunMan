1de42eff3d3174a1ca13856ca815747a42fa7a14
深瀬 貴将 <fukase@arsaga.jp>
2020-11-13 10:54:51 +0900

    add WorldRankingCell for customCell

---
+++ AR-GunMan/View/WorldRanking/WorldRankingCell.swift
@@ -0,0 +1,26 @@
+//
+//  WorldRankingCell.swift
+//  AR-GunMan
+//
+//  Created by 深瀬 貴将 on 2020/11/12.
+//
+
+import UIKit
+
+class WorldRankingCell: UITableViewCell {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var rankLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+}
