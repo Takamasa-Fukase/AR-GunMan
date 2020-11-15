4d8b39e73ec4a503b2e7d8840d47e81c97749bcf
深瀬 貴将 <fukase@arsaga.jp>
2020-11-15 15:18:31 +0900

    insert this time result to display tentative rank. change cell color at tentative rank index

--- AR-GunMan/View/WorldRanking/WorldRankingViewController.swift
+++ AR-GunMan/View/WorldRanking/WorldRankingViewController.swift
@@ -15,14 +15,20 @@ struct Ranking {
 
 class WorldRankingViewController: UIViewController {
 
+    var totalScore: Double = 0.000
+    var limitRankIndex = Int()
     var db: Firestore!
     var list: [Ranking] = []
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var totalScoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        totalScore = Double.random(in: 0...100)
+        
+        totalScoreLabel.text = String(format: "%.3f", totalScore)
         tableView.contentInset.top = 10
         tableView.register(UINib(nibName: "WorldRankingCell", bundle: nil), forCellReuseIdentifier: "WorldRankingCell")
         
@@ -35,7 +41,26 @@ class WorldRankingViewController: UIViewController {
             self.list = snapshot.documents.map { data -> Ranking in
                 return Ranking(score: data.data()["score"] as? Double ?? 0.000, userName: data.data()["user_name"] as? String ?? "NO NAME")
             }
+            
+//            if !self.list.isEmpty {
+                
+                let threeDigitsScore = Double(round(1000 * self.totalScore)/1000)
+                
+                let limitRankIndex = self.list.firstIndex(where: {
+                    $0.score < threeDigitsScore
+                })
+                self.limitRankIndex = limitRankIndex ?? 0
+                
+                self.list.insert(Ranking(score: threeDigitsScore, userName: "YOU"), at: limitRankIndex ?? 0)
+                
+//                print("list: \(self.list)")
+//                print("limitRankIndex: \(limitRankIndex)")
+                
+//            }
+            
             self.tableView.reloadData()
+            
+            print("reload()")
         }
         
         //背景をぼかし処理
@@ -51,6 +76,14 @@ class WorldRankingViewController: UIViewController {
         self.view.sendSubviewToBack(blueView)
         
         self.view.insertSubview(visualEffectView, at: 0)
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+            let storyboard: UIStoryboard = UIStoryboard(name: "RegisterNameViewController", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "RegisterNameViewController") as! RegisterNameViewController
+            vc.totalScore = self.totalScore
+            self.present(vc, animated: true)
+        }
 
     }
     
@@ -66,6 +99,18 @@ extension WorldRankingViewController: UITableViewDelegate, UITableViewDataSource
         cell?.nameLabel.text = list[indexPath.row].userName
         cell?.scoreLabel.text = String(list[indexPath.row].score)
         cell?.rankLabel.text = String(indexPath.row + 1)
+        
+        if indexPath.row == limitRankIndex {
+            
+            cell?.contentView.subviews[0].backgroundColor = UIColor(red: 255/255, green: 217/255, blue: 0/255, alpha: 1)
+            cell?.contentView.subviews[1].backgroundColor = UIColor(red: 255/255, green: 190/255, blue: 0/255, alpha: 1)
+        }else {
+            
+            cell?.contentView.subviews[0].backgroundColor = UIColor(red: 52/255, green: 78/255, blue: 130/255, alpha: 1)
+            cell?.contentView.subviews[1].backgroundColor = UIColor(red: 59/255, green: 89/255, blue: 148/255, alpha: 1)
+            
+        }
+        
         return cell ?? UITableViewCell()
     }
     
