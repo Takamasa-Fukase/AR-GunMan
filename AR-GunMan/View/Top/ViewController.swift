1b7f9612d27d0119482a953b812ef9320d195d83
深瀬 貴将 <fukase@arsaga.jp>
2021-02-27 01:07:34 +0900

    replace IBAction to Rx

--- AR-GunMan/View/Top/ViewController.swift
+++ AR-GunMan/View/Top/ViewController.swift
@@ -7,20 +7,78 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 import AVFoundation
 import PanModal
 
 class ViewController: UIViewController {
     
+    let disposeBag = DisposeBag()
     var replayFlag = false
     var pistolShoot = AVAudioPlayer()
     
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var howToPlayButton: UIButton!
     @IBOutlet weak var startButtonIcon: UIImageView!
     @IBOutlet weak var settingsButtonIcon: UIImageView!
     @IBOutlet weak var howToPlayButtonIcon: UIImageView!
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+               
+        if replayFlag {
+            presentGameVC(animated: false)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let _ = startButton.rx.tap
+            .subscribe(onNext: { [weak self] element in
+                guard let self = self else {return}
+                CameraAuthModel.checkCameraAuthorization(vc: self)
+                self.changeButtonIcon(self.startButtonIcon)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.presentGameVC()
+                }
+            }).disposed(by: disposeBag)
+        
+        let _ = settingsButton.rx.tap
+            .subscribe(onNext: { [weak self] element in
+                guard let self = self else {return}
+                self.changeButtonIcon(self.settingsButtonIcon)
+                
+                let storyboard: UIStoryboard = UIStoryboard(name: "SettingsViewController", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "SettingsViewController") as! SettingsViewController
+                let navi = UINavigationController(rootViewController: vc)
+                navi.setNavigationBarHidden(true, animated: false)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.presentPanModal(navi)
+                }
+            }).disposed(by: disposeBag)
+        
+        let _ = howToPlayButton.rx.tap
+            .subscribe(onNext: { [weak self] element in
+                guard let self = self else {return}
+                self.changeButtonIcon(self.howToPlayButtonIcon)
+                
+                let storyboard: UIStoryboard = UIStoryboard(name: "TutorialViewController", bundle: nil)
+                let vc = storyboard.instantiateViewController(withIdentifier: "TutorialViewController") as! TutorialViewController
+                
+                vc.isBlurEffectEnabled = false
+                
+                let navi = UINavigationController(rootViewController: vc)
+                navi.setNavigationBarHidden(true, animated: false)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.presentPanModal(navi)
+                }
+            }).disposed(by: disposeBag)
+        
         
         let audioSession = AVAudioSession.sharedInstance()
         
@@ -45,59 +103,6 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-               
-        if replayFlag {
-            presentGameVC(animated: false)
-        }
-    }
-
-    @IBAction func toGameButtonTapped(_ sender: Any) {
-                
-        CameraAuthModel.checkCameraAuthorization(vc: self)
-        
-        changeButtonIcon(startButtonIcon, isStartButtonTapped: true)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.presentGameVC()
-        }
-        
-    }
-    
-    @IBAction func toSettingButtonTapped(_ sender: Any) {
-        
-        changeButtonIcon(settingsButtonIcon)
-        
-        let storyboard: UIStoryboard = UIStoryboard(name: "SettingsViewController", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "SettingsViewController") as! SettingsViewController
-        let navi = UINavigationController(rootViewController: vc)
-        navi.setNavigationBarHidden(true, animated: false)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.presentPanModal(navi)
-        }
-    }
-    
-    @IBAction func toTutorialButtonTapped(_ sender: Any) {
-        
-        changeButtonIcon(howToPlayButtonIcon)
-        
-        let storyboard: UIStoryboard = UIStoryboard(name: "TutorialViewController", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "TutorialViewController") as! TutorialViewController
-        
-        vc.isBlurEffectEnabled = false
-        
-        let navi = UINavigationController(rootViewController: vc)
-        navi.setNavigationBarHidden(true, animated: false)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.presentPanModal(navi)
-        }
-    }
-    
-    
-    
     func presentGameVC(animated: Bool = true) {
         if #available(iOS 13.0, *) {
             startButtonIcon.image = UIImage(systemName: "target")
@@ -112,12 +117,9 @@ class ViewController: UIViewController {
     }
     
     
-    func changeButtonIcon(_ imageView: UIImageView, isStartButtonTapped: Bool = false) {
-        
+    func changeButtonIcon(_ imageView: UIImageView) {
         imageView.image = UIImage(named: "bulletsHole")
-        
         pistolShoot.play()
-        
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             if #available(iOS 13.0, *) {
                 imageView.image = UIImage(systemName: "target")
--- AR-GunMan/ViewModel/TopViewModel.swift
+++ AR-GunMan/ViewModel/TopViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class TopViewModel {
+    
+    //input
+//    var rankingWillAppear: AnyObserver<Void>
+    
+    
+    //output
+//    var dismissSwitchWeaponVC: Observable<Void>
+    
+    
+    init() {
+        
+//        let _dismissSwitchWeaponVC = PublishRelay<Void>()
+//        dismissSwitchWeaponVC = _dismissSwitchWeaponVC.asObservable()
+//
+//        rankingWillAppear = AnyObserver<Void>() { _ in
+//
+//            print("GameVM 武器選択を閉じる指示を流します")
+//
+//            _dismissSwitchWeaponVC.accept(Void())
+//        }
+        
+    }
+    
+    
+}
