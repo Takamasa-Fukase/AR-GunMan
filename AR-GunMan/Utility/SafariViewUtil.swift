2ea825d37c1fb0629d944e6aba83cdcf5960b6ea
Takamasa Fukase <takama224@yahoo.co.jp>
2022-01-30 22:40:36 +0400

    add SafariViewUtility class

---
+++ AR-GunMan/Utility/SafariViewUtil.swift
@@ -0,0 +1,16 @@
+//
+//  SafariViewUtil.swift
+//  AR-GunMan
+//
+//  Created by ウルトラ深瀬 on 2022/01/30.
+//
+
+import Foundation
+import SafariServices
+
+class SafariViewUtil {
+    static func openSafariView(urlString: String, vc: UIViewController) {
+        let safariVC = SFSafariViewController(url: NSURL(string: urlString)! as URL)
+        vc.present(safariVC, animated: true, completion: nil)
+    }
+}
