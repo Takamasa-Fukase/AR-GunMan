94c517065b93409605b464af5fad8df9a425005d
Fukase-Takamasa <ultrafukase@gmail.com>
2023-01-17 18:50:28 +0100

    add Rx extension for UIViewController life cycle methods

---
+++ AR-GunMan/Extension/UIViewController+RxLifeCycleEvents.swift
@@ -0,0 +1,59 @@
+//
+//  UIViewController+RxLifeCycleEvents.swift
+//  AR-GunMan
+//
+//  Created by ウルトラ深瀬 on 16/1/23.
+//
+
+import RxCocoa
+import RxSwift
+
+extension Reactive where Base: UIViewController {
+    var viewWillAppear: Observable<Void> {
+        return sentMessage(#selector(base.viewWillAppear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+    
+    var viewDidAppear: Observable<Void> {
+        return sentMessage(#selector(base.viewDidAppear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+    
+    var viewWillDisappear: Observable<Void> {
+        return sentMessage(#selector(base.viewWillDisappear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+    
+    var viewDidDisappear: Observable<Void> {
+        return sentMessage(#selector(base.viewDidDisappear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+
+    var viewWillAppearInvoked: Observable<Void> {
+        return methodInvoked(#selector(base.viewWillAppear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+    
+    var viewDidAppearInvoked: Observable<Void> {
+        return methodInvoked(#selector(base.viewDidAppear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+    
+    var viewWillDisappearInvoked: Observable<Void> {
+        return methodInvoked(#selector(base.viewWillDisappear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+    
+    var viewDidDisappearInvoked: Observable<Void> {
+        return methodInvoked(#selector(base.viewDidDisappear(_:)))
+            .map { _ in () }
+            .share(replay: 1)
+    }
+}
