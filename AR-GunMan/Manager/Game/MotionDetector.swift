f4ff711d4d43d88f10573403a0c2203d9b6eae36
Takamasa Fukase <ultrafukase@gmail.com>
2024-03-24 22:56:45 +0900

    不要コードを見直して整理

--- AR-GunMan/Manager/Game/MotionDetector.swift
+++ AR-GunMan/Manager/Game/MotionDetector.swift
@@ -13,13 +13,11 @@ class MotionDetector {
     let firingMotionDetected: Observable<Void>
     let reloadingMotionDetected: Observable<Void>
     let secretEventMotionDetected: Observable<Void>
-    
-    private var preBool = false
-    private var postBool = false
-    private var accele = CMAcceleration()
-    private var gyro = CMRotationRate()
+
     private let motionManager = CMMotionManager()
     private var gyroZcount = 0
+    // 発射動作の判定では加速度＋ジャイロも使うので、最新の値としてここに格納して使う
+    private var gyroCompositeValue: Double = 0.0
     
     init() {
         let firingMotionDetectedRelay = PublishRelay<Void>()
@@ -33,26 +31,42 @@ class MotionDetector {
         self.motionManager.accelerometerUpdateInterval = 0.2
         self.motionManager.startAccelerometerUpdates(to: OperationQueue.current!) {
             (data, error) in
+            guard let acceleration = data?.acceleration else { return }
+            let accelerationCompositeValue = self.getCompositeValue(
+                x: 0,
+                y: acceleration.y,
+                z: acceleration.z
+            )
             DispatchQueue.main.async {
-                guard let acceleration = data?.acceleration else { return }
-                self.accele = acceleration
-                self.didUpdateAccelerationData(data: acceleration) {
-                    firingMotionDetectedRelay.accept(Void())
-                }
+                self.handleUpdatedAccelerationData(
+                    compositeValue: accelerationCompositeValue,
+                    gyroZSquaredValue: self.gyroCompositeValue,
+                    completion: {
+                        firingMotionDetectedRelay.accept(Void())
+                    }
+                )
             }
         }
         
         self.motionManager.gyroUpdateInterval = 0.2
         self.motionManager.startGyroUpdates(to: OperationQueue.current!) {
             (data, error) in
+            guard let rotationRate = data?.rotationRate else { return }
+            // 加速度の方の判定でジャイロも使うので格納する
+            self.gyroCompositeValue = self.getCompositeValue(
+                x: 0,
+                y: 0,
+                z: rotationRate.z
+            )
             DispatchQueue.main.async {
-                guard let rotationRate = data?.rotationRate else { return }
-                self.gyro = rotationRate
-                self.didUpdateGyroData(data: rotationRate, completion: {
-                    reloadingMotionDetectedRelay.accept(Void())
-                }) {
-                    secretEventMotionDetectedRelay.accept(Void())
-                }
+                self.handleUpdatedGyroData(
+                    compositeValue: self.gyroCompositeValue,
+                    completion: {
+                        reloadingMotionDetectedRelay.accept(Void())
+                    }, secretEvent: {
+                        secretEventMotionDetectedRelay.accept(Void())
+                    }
+                )
             }
         }
     }
@@ -63,61 +77,31 @@ class MotionDetector {
     }
     
     //MARK: - Private Methods
-    private func didUpdateAccelerationData(data: CMAcceleration, completion: (() -> ())?) {
-        let compositAcceleration = getCompositeAcceleration(0, data.y, data.z)
-        
-        let gyroZ = (gyro.z * gyro.z)
-        
-        //連続動作の内の初回
-        if !postBool
-            && compositAcceleration >= 1.5
-            && gyroZ < 10 {
-            
-            preBool = true
-            
-            //武器を発動
-            completion?()
-        }
-        
-        //連続動作の内の2回目以降
-        if postBool
-            && compositAcceleration >= 1.5
-            && gyroZ < 10 {
-            
-            postBool = false
-            preBool = false
-            
-            //武器を発動
-            completion?()
+    private func handleUpdatedAccelerationData(
+        compositeValue: Double,
+        gyroZSquaredValue: Double,
+        completion: () -> Void
+    ) {
+        if compositeValue >= 1.5 && gyroZSquaredValue < 10 {
+            completion()
         }
     }
     
-    private func didUpdateGyroData(data: CMRotationRate, completion: (() -> ())?, secretEvent: (() -> ())?) {
-        let compositGyro = getCompositeGyro(0, 0, gyro.z)
-
-        if compositGyro >= 10 {
-            
-            completion?()
-            
+    private func handleUpdatedGyroData(
+        compositeValue: Double,
+        completion: () -> Void,
+        secretEvent: () -> Void
+    ) {
+        if compositeValue >= 10 {
+            completion()
             gyroZcount += 1
-            print("gyroZcountを+1。 現在: \(gyroZcount)回")
-            
         }
-        
         if gyroZcount == 20 {
-            
-            print("gyroZcountが20に達したのでターゲットを泰明さんに変えます")
-            
-            secretEvent?()
+            secretEvent()
         }
-
-    }
-        
-    private func getCompositeAcceleration(_ x: Double, _ y: Double, _ z: Double) -> Double {
-        return (x * x) + (y * y) + (z * z)
     }
     
-    private func getCompositeGyro(_ x: Double, _ y: Double, _ z: Double) -> Double {
+    private func getCompositeValue(x: Double, y: Double, z: Double) -> Double {
         return (x * x) + (y * y) + (z * z)
     }
 }
