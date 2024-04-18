945f28965bdd440dea86d3cc713e48f5d634c9f6
Takamasa Fukase <ultrafukase@gmail.com>
2024-04-19 00:38:43 +0900

    RankingRepositoryのメソッドを、Observableと繋げやすくする為にasync/awaitからSingleを返却する様に差し替え。また、別の場所でまだasync/await実装になっている箇所があるので一旦共存。

--- AR-GunMan/Repository/RankingRepository.swift
+++ AR-GunMan/Repository/RankingRepository.swift
@@ -34,4 +34,51 @@ class RankingRepository {
             .document()
             .setData(dict)
     }
+    
+    func getRanking2() -> Single<[Ranking]> {
+        return Single.create { [weak self] observer in
+            self?.firestoreDataBase
+                .collection("worldRanking")
+                .order(by: "score", descending: true)
+                .getDocuments { querySnapshot, error in
+                    guard let querySnapshot = querySnapshot else {
+                        observer(.failure(error ?? NSError()))
+                        return
+                    }
+                    let rankings = querySnapshot
+                        .documents
+                        .compactMap({ queryDocSnapshot in
+                            return try? queryDocSnapshot.data(as: Ranking.self)
+                        })
+                    observer(.success(rankings))
+                }
+            return Disposables.create()
+        }
+    }
+    
+    func registerRanking2(_ ranking: Ranking) -> Single<Void> {
+        // TODO: 何とかもう少し綺麗にしたい
+        return Single.create { [weak self] observer in
+            do {
+                let data = try JSONEncoder().encode(ranking)
+                if let dict = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any] {
+                    self?.firestoreDataBase
+                        .collection("worldRanking")
+                        .document()
+                        .setData(dict) { error in
+                            if let error = error {
+                                observer(.failure(error))
+                            }else {
+                                observer(.success(Void()))
+                            }
+                        }
+                }else {
+                    observer(.failure(NSError()))
+                }
+            } catch {
+                observer(.failure(error))
+            }
+            return Disposables.create()
+        }
+    }
 }
