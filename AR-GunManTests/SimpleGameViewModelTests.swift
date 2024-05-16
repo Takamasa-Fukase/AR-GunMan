eff0d8b2f8401cbd42ce34c3d5b5a609435c9c26
Fukase-Takamasa <ultrafukase@gmail.com>
2024-05-16 20:08:56 +0900

    SimpleGameVMのテストケースを追加

--- AR-GunManTests/SimpleGameViewModelTests.swift
+++ AR-GunManTests/SimpleGameViewModelTests.swift
@@ -26,12 +26,86 @@ final class SimpleGameViewModelTests: XCTestCase {
         super.tearDown()
     }
     
-    func test_pistolを7発撃った後に撃とうとしてもrenderWeaponFiringのイベントが流れなければ成功() {
+    func test_pistolの残弾数が0の時に3回撃とうとしてもrenderWeaponFiringイベントが流れなければ成功() {
         let state = SimpleGameViewModel.State()
+        // 残弾数を0にする
+        state.bulletsCountRelay.accept(0)
+        let viewModel = SimpleGameViewModel(state: state)
+        
+        let tryFiring3Times = scheduler.createHotObservable([
+            .next(0, ()),
+            .next(100, ()),
+            .next(200, ())
+        ])
+        let input = SimpleGameViewModel.Input(
+            inputFromGameScene: .init(targetHit: .empty()),
+            inputFromCoreMotion: .init(
+                firingMotionDetected: tryFiring3Times.asObservable(),
+                reloadingMotionDetected: .empty()
+            )
+        )
+        let output = viewModel.transform(input: input)
+        // subscribeしないと動かないVM内部の処理を通常通り動かす為に一括でsubscribe
+        subscribeAllVMActionEvents(output.viewModelAction)
+        
+        let renderWeaponFiringObserver = scheduler.createObserver(WeaponType.self)
+        output.outputToGameScene.renderWeaponFiring
+            .subscribe(renderWeaponFiringObserver)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(renderWeaponFiringObserver.events, [])
+    }
+    
+    func test_pistolの残弾数が0の時に3回撃とうとしても0のまま変わらなければ成功() {
+        let state = SimpleGameViewModel.State()
+        // 残弾数を0にする
+        state.bulletsCountRelay.accept(0)
+        let viewModel = SimpleGameViewModel(state: state)
+        
+        let tryFiring3Times = scheduler.createHotObservable([
+            .next(100, ()),
+            .next(200, ()),
+            .next(300, ())
+        ])
+        let input = SimpleGameViewModel.Input(
+            inputFromGameScene: .init(targetHit: .empty()),
+            inputFromCoreMotion: .init(
+                firingMotionDetected: tryFiring3Times.asObservable(),
+                reloadingMotionDetected: .empty()
+            )
+        )
+        let output = viewModel.transform(input: input)
+        // subscribeしないと動かないVM内部の処理を通常通り動かす為に一括でsubscribe
+        subscribeAllVMActionEvents(output.viewModelAction)
+        
+        let bulletsCountObserver = scheduler.createObserver(Int.self)
+        state.bulletsCountRelay
+            .subscribe(bulletsCountObserver)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        let expectedEvents: [Recorded] = [
+            // bulletsCountRelayはBehaviorRelayな為subscribe時に現在値が一回流れるのでそれも含める
+            .next(0, 0),
+        ]
+        
+        print("実際のイベント: \(bulletsCountObserver.events)")
+        print("期待値: \(expectedEvents)")
+        
+        XCTAssertEqual(bulletsCountObserver.events, expectedEvents)
+    }
+    
+    func test_pistolの残弾数がMAXの7発の時に10回撃とうとしてもrenderWeaponFiringのイベントが7回しか流れなければ成功() {
+        let state = SimpleGameViewModel.State()
+        // 残弾数をMAXの7発にする
+        state.bulletsCountRelay.accept(7)
         let viewModel = SimpleGameViewModel(state: state)
         
         // pistolの装弾数（7発）を超えて撃つイベントを作成
-        let fire10times = scheduler.createHotObservable([
+        let tryFiring10Times = scheduler.createHotObservable([
             .next(100, ()),
             .next(200, ()),
             .next(300, ()),
@@ -49,11 +123,13 @@ final class SimpleGameViewModelTests: XCTestCase {
                 targetHit: Observable.empty()
             ),
             inputFromCoreMotion: SimpleGameViewModel.Input.InputFromCoreMotion(
-                firingMotionDetected: fire10times.asObservable(),
+                firingMotionDetected: tryFiring10Times.asObservable(),
                 reloadingMotionDetected: Observable.empty()
             )
         )
         let output = viewModel.transform(input: input)
+        // subscribeしないと動かないVM内部の処理を通常通り動かす為に一括でsubscribe
+        subscribeAllVMActionEvents(output.viewModelAction)
         
         let renderWeaponFiringObserver = scheduler.createObserver(WeaponType.self)
         output.outputToGameScene.renderWeaponFiring
