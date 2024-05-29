dc8e5e348484c884095f730b8cf8b42a4966c61d
Fukase-Takamasa <ultrafukase@gmail.com>
2024-05-29 10:13:24 +0900

    同様にReloadingMotionFilterを追加

---
+++ AR-GunMan/Presentation/Screens/Game3/EventHandler/ReloadingMotionFilter.swift
@@ -0,0 +1,31 @@
+//
+//  ReloadingMotionFilter.swift
+//  AR-GunMan
+//
+//  Created by ウルトラ深瀬 on 29/5/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class ReloadingMotionFilter {
+    struct Input {
+        let gyroUpdated: Observable<(x: Double, y: Double, z: Double)>
+    }
+    
+    struct Output {
+        let reloadingMotionDetected: Observable<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        let reloadingMotionDetected = input.gyroUpdated
+            .map{ gyro in
+                return CompositeCalculator.getCompositeValue(x: 0, y: 0, z: gyro.z)
+            }
+            .filter { gyroCompositeValue in
+                gyroCompositeValue >= 10
+            }
+            .map({_ in})
+        return Output(reloadingMotionDetected: reloadingMotionDetected)
+    }
+}
