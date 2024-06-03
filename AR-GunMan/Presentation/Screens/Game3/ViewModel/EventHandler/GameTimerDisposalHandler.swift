ac5c087f4871888d9eb35d46b0033a14c5178a23
Fukase-Takamasa <ultrafukase@gmail.com>
2024-06-03 23:22:22 +0900

    GameTimerDisposalHandlerを追加

---
+++ AR-GunMan/Presentation/Screens/Game3/ViewModel/EventHandler/GameTimerDisposalHandler.swift
@@ -0,0 +1,45 @@
+//
+//  GameTimerDisposalHandler.swift
+//  AR-GunMan
+//
+//  Created by ウルトラ深瀬 on 3/6/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class GameTimerDisposalHandler {
+    struct Input {
+        let timerDisposed: Observable<Void>
+    }
+    
+    struct Output {
+        let stopMotionDetection: Observable<Void>
+        let dismissWeaponChangeView: Observable<Void>
+        let playRankingAppearSound: Observable<SoundType>
+        let showResultView: Observable<Void>
+    }
+    
+    private let gameUseCase: GameUseCase2Interface
+    
+    init(gameUseCase: GameUseCase2Interface) {
+        self.gameUseCase = gameUseCase
+    }
+    
+    func transform(input: Input) -> Output {
+        let resultViewShowingSignalReceived = input.timerDisposed
+            .flatMapLatest({ [weak self] _ -> Observable<Void> in
+                guard let self = self else { return .empty() }
+                return self.gameUseCase.awaitShowResultSignal()
+            })
+            .share()
+        
+        return Output(
+            stopMotionDetection: input.timerDisposed,
+            dismissWeaponChangeView: input.timerDisposed,
+            playRankingAppearSound: resultViewShowingSignalReceived
+                .map({ _ in .rankingAppear }),
+            showResultView: resultViewShowingSignalReceived
+        )
+    }
+}
