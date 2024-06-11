523e26694b60a939f1c6d7cb6ea47b1ee546887c
Fukase-Takamasa <ultrafukase@gmail.com>
2024-06-11 15:09:07 +0900

    TopVMのカメラ権限周りのハンドリングをするCameraPermissionHandlerを追加

---
+++ AR-GunMan/Presentation/Screens/Top/ViewModel/EventHandler/CameraPermissionHandler.swift
@@ -0,0 +1,48 @@
+//
+//  CameraPermissionHandler.swift
+//  AR-GunMan
+//
+//  Created by ウルトラ深瀬 on 11/6/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class CameraPermissionHandler: ViewModelEventHandlerType {
+    struct Input {
+        let checkIsCameraAccessPermitted: Observable<Void>
+    }
+    
+    struct Output {
+        let showGame: Observable<Void>
+        let showCameraPermissionDescriptionAlert: Observable<Void>
+    }
+    
+    private let topUseCase: TopUseCaseInterface2
+    
+    init(topUseCase: TopUseCaseInterface2) {
+        self.topUseCase = topUseCase
+    }
+    
+    func transform(input: Input) -> Output {
+        let isPermitted = input.checkIsCameraAccessPermitted
+            .flatMapLatest({ [weak self] _ -> Observable<Bool> in
+                guard let self = self else { return .empty() }
+                return self.topUseCase.getIsCameraAccessPermitted()
+            })
+            .share()
+        
+        let showGame = isPermitted
+            .filter({ $0 })
+            .map({ _ in })
+        
+        let showCameraPermissionDescriptionAlert = isPermitted
+            .filter({ !$0 })
+            .map({ _ in })
+        
+        return Output(
+            showGame: showGame,
+            showCameraPermissionDescriptionAlert: showCameraPermissionDescriptionAlert
+        )
+    }
+}
