75a106db8a4bac36a70d2b009b23beeaf82ef5e0
Fukase-Takamasa <ultrafukase@gmail.com>
2024-06-13 20:26:14 +0900

    Result画面とRanking画面で同じUIになっているランキングリスト箇所（それぞれStoryboard上で別々に実装されている）を共通のXibとしてカスタムビュークラスを作成。また、引数でrankingListとisLoadingのObservableを受け取ってUIパーツにbindする処理なども移植。

---
+++ AR-GunMan/Presentation/Screens/Ranking/View/RankingListView.swift
@@ -0,0 +1,63 @@
+//
+//  RankingListView.swift
+//  AR-GunMan
+//
+//  Created by ウルトラ深瀬 on 13/6/24.
+//
+
+import RxSwift
+import RxCocoa
+
+final class RankingListView: UIView {
+    private let disposeBag = DisposeBag()
+    
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var activityIndicatorView: UIActivityIndicatorView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    func bind(
+        rankingList: Observable<[Ranking]>,
+        isLoading: Observable<Bool>
+    ) {
+        rankingList
+            .bind(to: tableView.rx.items(
+                cellIdentifier: RankingCell.className,
+                cellType: RankingCell.self
+            )) { row, element, cell in
+                cell.configure(ranking: element, row: row)
+            }.disposed(by: disposeBag)
+        
+        isLoading
+            .subscribe(onNext: { [weak self] element in
+                guard let self = self else {return}
+                if element {
+                    self.activityIndicatorView.startAnimating()
+                }else {
+                    self.activityIndicatorView.stopAnimating()
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    func scrollCellToCenterVertically(at indexPath: IndexPath) {
+        tableView.scrollToRow(at: indexPath, at: .middle, animated: true)
+    }
+    
+    private func configure() {
+        let nib = UINib(nibName: RankingListView.className, bundle: nil)
+        guard let view = nib.instantiate(withOwner: self).first as? UIView else { return }
+        addSubview(view)
+        addConstraints(for: view)
+        
+        tableView.contentInset.top = 10
+        tableView.register(UINib(nibName: RankingCell.className, bundle: nil), forCellReuseIdentifier: RankingCell.className)
+    }
+}
