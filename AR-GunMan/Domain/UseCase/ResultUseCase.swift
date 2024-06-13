f804338ce3d11a751920b1a13e562b0f9b447ce4
Fukase-Takamasa <ultrafukase@gmail.com>
2024-06-13 11:30:28 +0900

    ResultUseCaseに結果画面で名前登録ダイアログを表示するまでの待機時間をawaitするメソッドを追加

--- AR-GunMan/Domain/UseCase/ResultUseCase.swift
+++ AR-GunMan/Domain/UseCase/ResultUseCase.swift
@@ -9,23 +9,36 @@ import RxSwift
 
 protocol ResultUseCaseInterface {
     func getRanking() -> Single<[Ranking]>
+    func awaitShowNameRegisterSignal() -> Observable<Void>
     func setNeedsReplay(_ newValue: Bool) -> Observable<Void>
 }
 
 final class ResultUseCase: ResultUseCaseInterface {
     private let rankingRepository: RankingRepositoryInterface
+    private let timerRepository: TimerRepositoryInterface
     private let replayRepository: ReplayRepositoryInterface
     
     init(
         rankingRepository: RankingRepositoryInterface,
+        timerRepository: TimerRepositoryInterface,
         replayRepository: ReplayRepositoryInterface
     ) {
         self.rankingRepository = rankingRepository
+        self.timerRepository = timerRepository
         self.replayRepository = replayRepository
     }
     
     func getRanking() -> Single<[Ranking]> {
         return rankingRepository.getRanking()
+    }
+    
+    func awaitShowNameRegisterSignal() -> Observable<Void> {
+        return timerRepository
+            .getTimerStream(
+                milliSec: ResultConst.showNameRegisterWaitingTimeMillisec,
+                isRepeated: false
+            )
+            .map({ _ in })
     }
     
     func setNeedsReplay(_ newValue: Bool) -> Observable<Void> {
