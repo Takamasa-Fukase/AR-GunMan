e9ce8bb04ab27f39233543a25f5e1e841e5b1a45
Fukase-Takamasa <ultrafukase@gmail.com>
2024-06-18 20:19:23 +0900

    TimerStreamCreatorを追加

--- AR-GunMan/Utilities/TimerStreamCreator.swift
+++ AR-GunMan/Utilities/TimerStreamCreator.swift
@@ -1,19 +1,14 @@
 //
-//  TimerRepository.swift
+//  TimerStreamCreator.swift
 //  AR-GunMan
 //
-//  Created by 深瀬 on 2024/05/07.
+//  Created by ウルトラ深瀬 on 18/6/24.
 //
 
 import RxSwift
 
-// TODO: - 差し替えが終わったらこのファイルを削除する
-protocol TimerRepositoryInterface {
-    func getTimerStream(milliSec: Int, isRepeated: Bool) -> Observable<Int>
-}
-
-final class TimerRepository: TimerRepositoryInterface {
-    func getTimerStream(milliSec: Int, isRepeated: Bool) -> Observable<Int> {
+final class TimerStreamCreator {
+    static func create(milliSec: Int, isRepeated: Bool) -> Observable<Int> {
         // 返却されるInt値は、タイマーが更新された回数。1ずつ加算された値がStreamで返却される。
         return Observable<Int>
             .timer(
