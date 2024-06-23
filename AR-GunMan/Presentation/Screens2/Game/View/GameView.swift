6313cff0f6a7946cb6e2b2781e7ddcb08fa3c6b1
Fukase-Takamasa <ultrafukase@gmail.com>
2024-06-23 17:03:16 +0900

    GameVCのPassiveView部分を単独のXibに切り出し

---
+++ AR-GunMan/Presentation/Screens2/Game/View/GameView.swift
@@ -0,0 +1,42 @@
+//
+//  GameView.swift
+//  AR-GunMan
+//
+//  Created by ウルトラ深瀬 on 23/6/24.
+//
+
+import UIKit
+
+final class GameView: UIView {
+    @IBOutlet weak var bulletsCountImageView: UIImageView!
+    @IBOutlet weak var sightImageView: UIImageView!
+    @IBOutlet weak var timeCountLabel: UILabel!
+    @IBOutlet weak var weaponChangeButton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        loadNib()
+        setupUI()
+    }
+    
+    private func loadNib() {
+        let nib = UINib(nibName: GameView.className, bundle: nil)
+        guard let view = nib.instantiate(withOwner: self).first as? UIView else { return }
+        addSubview(view)
+        addConstraints(for: view)
+    }
+    
+    private func setupUI() {
+        // MEMO: to prevent time count text looks shaking horizontally rapidly.
+        timeCountLabel.font = timeCountLabel.font.monospacedDigitFont
+    }
+}
