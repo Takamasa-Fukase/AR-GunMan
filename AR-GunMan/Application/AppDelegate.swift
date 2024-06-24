cd8b447697807515ab89b4fbe530cdcef368f7a7
Fukase-Takamasa <ultrafukase@gmail.com>
2024-06-24 19:43:43 +0900

    SceneDelegateを追加し、storyboardを使わない設定に差し替え

--- AR-GunMan/Application/AppDelegate.swift
+++ AR-GunMan/Application/AppDelegate.swift
@@ -14,7 +14,6 @@ import AVFoundation
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
     let gcmMessageIDKey = "gcm.message_id"
 
 
@@ -50,9 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         
         AVCaptureDevice.requestAccess(for: .video) { _ in }
-        
-        window?.rootViewController = TopNavigator2.assembleModules()
-        
+                
         return true
     }
     
@@ -73,7 +70,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
     
-    
+    // MARK: UISceneSession Lifecycle
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
     
     // [START receive_message]
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
