f9604679d3465ad3f397590b916c370a77d7b69f
Takamasa-Fukase <ultrafukase@gmail.com>
2025-01-22 17:49:41 +0900

    FSPagerViewを使っていたクラスをコメントアウト。一旦ビルドが通る様にした。

--- AR-GunMan/Presentation/WeaponSelect/WeaponSelectCell.swift
+++ AR-GunMan/Presentation/WeaponSelect/WeaponSelectCell.swift
@@ -6,22 +6,22 @@
 //  Copyright © 2020 fukase. All rights reserved.
 //
 
-import UIKit
-import FSPagerView
-
-final class WeaponSelectCell: FSPagerViewCell {
-    @IBOutlet private weak var weaponImageView: UIImageView!
-    @IBOutlet private weak var commingSoonLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
-    func configure(
-        weaponImage: UIImage?,
-        isHiddenCommingSoonLabel: Bool
-    ) {
-        weaponImageView.image = weaponImage
-        commingSoonLabel.isHidden = isHiddenCommingSoonLabel
-    }
-}
+//import UIKit
+//import FSPagerView
+//
+//final class WeaponSelectCell: FSPagerViewCell {
+//    @IBOutlet private weak var weaponImageView: UIImageView!
+//    @IBOutlet private weak var commingSoonLabel: UILabel!
+//    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//    }
+//    
+//    func configure(
+//        weaponImage: UIImage?,
+//        isHiddenCommingSoonLabel: Bool
+//    ) {
+//        weaponImageView.image = weaponImage
+//        commingSoonLabel.isHidden = isHiddenCommingSoonLabel
+//    }
+//}
