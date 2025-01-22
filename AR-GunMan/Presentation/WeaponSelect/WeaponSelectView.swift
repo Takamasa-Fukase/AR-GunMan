9e5d52d43352616c5c01736412e3923d8a2cd3e9
Takamasa-Fukase <ultrafukase@gmail.com>
2025-01-22 21:10:27 +0900

    武器選択画面のUIを仮作成

--- AR-GunMan/Presentation/WeaponSelect/WeaponSelectView.swift
+++ AR-GunMan/Presentation/WeaponSelect/WeaponSelectView.swift
@@ -13,42 +13,30 @@ struct WeaponSelectView: View {
     let weaponSelected: (Int) -> Void
     
     var body: some View {
-        //        WeaponSelectViewControllerRepresentable(
-        //            weaponSelected: weaponSelected,
-        //            weaponListItems: viewModel.weaponListItems
-        //        )
-        Text("Weapon Select")
-            .onAppear {
-                viewModel.onViewAppear()
+        ContentFrameTrackableScrollView(
+            scrollDirections: .horizontal,
+            showsIndicator: false,
+            content: {
+                HStack(spacing: 0) {
+                    ForEach(Array(viewModel.weaponListItems.enumerated()), id: \.offset) { index, item in
+                        Image(item.weaponImageName)
+                            .resizable()
+                            .frame(width: 200, height: 200)
+                            .aspectRatio(contentMode: .fit)
+                            .id(index)
+                    }
+                }
+            },
+            onScroll: { contentFrame in
+                
             }
+        )
+        .onAppear {
+            viewModel.onViewAppear()
+        }
     }
 }
 
-//struct WeaponSelectViewControllerRepresentable: UIViewControllerRepresentable {
-//    private var weaponSelected: (Int) -> Void
-//    private var weaponListItems: [WeaponListItem]
-//    @Environment(\.dismiss) private var dismiss
-//    
-//    init(
-//        weaponSelected: @escaping (Int) -> Void,
-//        weaponListItems: [WeaponListItem]
-//    ) {
-//        self.weaponSelected = weaponSelected
-//        self.weaponListItems = weaponListItems
-//    }
-//    
-//    func makeUIViewController(context: Context) -> WeaponSelectViewController {
-//        let weaponSelectVC = WeaponSelectViewController()
-//        weaponSelectVC.weaponSelected = { weaponId in
-//            // このViewの利用側へ選択されたweaponIdをコールバック
-//            weaponSelected(weaponId)
-//            // 画面を閉じる
-//            dismiss()
-//        }
-//        return weaponSelectVC
-//    }
-//    
-//    func updateUIViewController(_ uiViewController: WeaponSelectViewController, context: Context) {
-//        uiViewController.updateWeaponListItems(weaponListItems)
-//    }
-//}
+#Preview {
+    WeaponSelectViewFactory.create(weaponSelected: {_ in })
+}
