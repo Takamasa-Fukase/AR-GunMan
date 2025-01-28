46ac50a4777e4bac65cae9082db8ba1a4b0f8f3d
Takamasa-Fukase <ultrafukase@gmail.com>
2025-01-29 08:42:00 +0900

    AppDelegateにビルド環境毎のFirebaseの設定処理を追加

--- AR-GunMan/App/AppDelegate.swift
+++ AR-GunMan/App/AppDelegate.swift
@@ -6,10 +6,28 @@
 //
 
 import UIKit
+import FirebaseCore
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         SoundPlayer.shared.setup()
+        setupFirebaseApp()
         return true
+    }
+    
+    // 環境毎の設定を行う
+    private func setupFirebaseApp() {
+        let configFileName: String = {
+#if DEBUG
+            return "GoogleService-Info.dev"
+#else
+            return "GoogleService-Info"
+#endif
+        }()
+        guard let filePath = Bundle.main.path(forResource: configFileName, ofType: "plist"),
+              let options = FirebaseOptions(contentsOfFile: filePath) else {
+            fatalError("FirebaseInfoPlist file was not found")
+        }
+        FirebaseApp.configure(options: options)
     }
 }
