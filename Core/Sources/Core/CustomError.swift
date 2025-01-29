2e73c6cdf8e879f5437f74ca8c831eba44dee5d5
Takamasa-Fukase <ultrafukase@gmail.com>
2025-01-29 20:45:27 +0900

    ErrorConstをCoreモジュールに移動し、CustomErrorのcase毎に共通エラー表示用のtitle, messageプロパティを追加

--- Core/Sources/Core/CustomError.swift
+++ Core/Sources/Core/CustomError.swift
@@ -11,4 +11,20 @@ public enum CustomError: Error {
     case apiClientError(Error)
     case networkError(Error)
     case other(message: String)
+    
+    public var title: String {
+        switch self {
+        default:
+            return ErrorConst.defaultAlertTitle
+        }
+    }
+    
+    public var message: String {
+        switch self {
+        case .apiClientError(let error), .networkError(let error):
+            return error.localizedDescription
+        case .other(message: let message):
+            return message
+        }
+    }
 }
---
+++ Core/Sources/Core/ErrorConst.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorConst.swift
+//  Core
+//
+//  Created by 深瀬 on 2024/04/24.
+//
+
+import Foundation
+
+public final class ErrorConst {
+    public static let defaultAlertTitle = "Whoops, something went wrong!"
+    public static let defaultCloseButtonTitle = "Close"
+    public static let unknownErrorMessage = "Unknown error occurred."
+}
