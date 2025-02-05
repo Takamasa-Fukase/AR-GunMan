30068942b5965e5268415b6d02103a5e41eff0cf
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-05 15:15:28 +0900

    メインプロジェクトでのユニットテストのビルドが通るように修正

--- AR-GunManTests/GameViewModelTests.swift
+++ AR-GunManTests/GameViewModelTests.swift
@@ -6,16 +6,25 @@
 //
 
 import XCTest
-@testable import AR_GunMan
+@testable import AR_GunMan_Dev
 
 final class GameViewModelTests: XCTestCase {
-//    private var gameViewModel:
+    private var gameViewModel: GameViewModel!
     
     override func setUp() {
-        
+        gameViewModel = .init(
+            tutorialRepository: RepositoryFactory.create(),
+            gameTimerCreateUseCase: UseCaseFactory.create(),
+            weaponResourceGetUseCase: UseCaseFactory.create(),
+            weaponActionExecuteUseCase: UseCaseFactory.create()
+        )
     }
     
     override func tearDown() {
-        
+        gameViewModel = nil
+    }
+    
+    func test_example() {
+        XCTAssertEqual(gameViewModel.score, 0.0)
     }
 }
