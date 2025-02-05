aa3df9f85f24515116c476dff6acf3f1c5e32d21
Takamasa-Fukase <ultrafukase@gmail.com>
2025-02-05 13:07:40 +0900

    GameViewModelのユニットテストファイルを追加

---
+++ AR-GunManTests/GameViewModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  GameViewModelTests.swift
+//  AR-GunManTests
+//
+//  Created by ウルトラ深瀬 on 5/2/25.
+//
+
+import XCTest
+@testable import AR_GunMan
+
+final class GameViewModelTests: XCTestCase {
+//    private var gameViewModel:
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+}
