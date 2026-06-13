e70a134ee1d763da0b61a30a22c3a42d905dd809
Takamasa-Fukase <ultrafukase@gmail.com>
2026-06-13 09:34:32 +0900

    ARShootingLibパッケージ内にDemo用のアプリを追加

---
+++ ARShootingLib/DemoApp/ARShootingLibDemoApp/ARShootingLibDemoApp/ARShootingLibDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARShootingLibDemoAppApp.swift
+//  ARShootingLibDemoApp
+//
+//  Created by ウルトラ深瀬 on 2026/06/13.
+//
+
+import SwiftUI
+
+@main
+struct ARShootingLibDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARShootingLib/DemoApp/ARShootingLibDemoApp/ARShootingLibDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARShootingLibDemoApp
+//
+//  Created by ウルトラ深瀬 on 2026/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
