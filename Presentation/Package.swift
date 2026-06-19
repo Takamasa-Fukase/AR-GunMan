738ca56129b189f2255c60110dd5f0e51746311d
Takamasa-Fukase <ultrafukase@gmail.com>
2026-06-19 20:06:42 +0900

    パッケージへの依存追記など

--- Presentation/Package.swift
+++ Presentation/Package.swift
@@ -1,27 +1,29 @@
-// swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "Presentation",
+    platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Presentation",
             targets: ["Presentation"]
         ),
     ],
+    dependencies: [
+        .package(path: "Domain"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Presentation"
+            name: "Presentation",
+            dependencies: [
+                "Domain",
+            ],
         ),
         .testTarget(
             name: "PresentationTests",
             dependencies: ["Presentation"]
         ),
     ],
-    swiftLanguageModes: [.v6]
 )
---
+++ Presentation/Sources/Presentation/Game/GamePresenter.swift
@@ -0,0 +1,9 @@
+//
+//  GamePresenter.swift
+//  Presentation
+//
+//  Created by ウルトラ深瀬 on 2026/06/19.
+//
+
+import Foundation
+
