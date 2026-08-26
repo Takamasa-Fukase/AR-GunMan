9003970f974e3a5d66f87fa7f6296a03842f5e60
Takamasa-Fukase <ultrafukase@gmail.com>
2026-08-26 21:44:15 +0900

    依存として登録など

--- ARShootingEngine/Package.swift
+++ ARShootingEngine/Package.swift
@@ -1,20 +1,17 @@
-// swift-tools-version: 6.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.10
 
 import PackageDescription
 
 let package = Package(
     name: "ARShootingEngine",
+    platforms: [.iOS(.v17)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ARShootingEngine",
             targets: ["ARShootingEngine"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ARShootingEngine"
         ),
@@ -23,5 +20,4 @@ let package = Package(
             dependencies: ["ARShootingEngine"]
         ),
     ],
-    swiftLanguageModes: [.v6]
 )
